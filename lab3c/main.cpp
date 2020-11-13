72966ee373465d967baa33d40d40a586041b2182
aestepanova <anchousannstep@gmail.com>
2020-11-14 02:16:13 +0300

    let's start!

---
+++ lab3c/main.cpp
@@ -0,0 +1,66 @@
+#include "src/bigNum.h"
+
+using namespace Prog3b;
+
+int main() {
+    bigNum first, second;
+    int go=1;
+    while (go) {
+        std::cout << "Enter a - the first decimal number: ";
+        try {
+            std::cin>>first;
+        }
+        catch (const std::exception &msg) {
+            std::cout << msg.what() << std::endl;
+        }
+
+        std::cout << "Enter b - the second decimal number: ";
+        try {
+            std::cin>>second;
+        }
+        catch (const std::exception &msg) {
+            std::cout << msg.what() << std::endl;
+        }
+
+        try{
+            std::cout << "a + b = " << first+second << std::endl;
+            std::cout << "a - b = " << first-second << std::endl;
+            std::cout << "b - a = " << second-first << std::endl;
+        }
+        catch (const std::exception &msg) {
+            std::cout << msg.what() << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        try{
+            std::cout << "a = " << first << std::endl;
+            std::cout << "Add code for number a: " << (~first) << std::endl;
+            std::cout << "Increase number a by 10 times: " << (first>>=1) << std::endl;
+            std::cout << "Decrease number a by 10 times: " << (first<<=2) << std::endl;
+        }
+        catch (const std::exception &msg) {
+            std::cout << msg.what() << std::endl;
+        }
+
+        std::cout << std::endl;
+
+        try{
+            std::cout << "b = " << second << std::endl;
+            std::cout << "Add code for number b: " << (~second) << std::endl;
+            std::cout << "Increase number b by 10 times: " << (second>>=1) << std::endl;
+            std::cout << "Decrease number b by 10 times: " << (second<<=2) << std::endl;
+        }
+        catch (const std::exception &msg) {
+            std::cout << msg.what() << std::endl;
+        }
+
+        std::cout << std::endl;
+        std::cout << "Enter 0 to exit or 1 to continue: ";
+        std::cin >> go;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    std::cout << "Goodbye!" << std::endl;
+
+    return 0;
+}
---
+++ lab3c/src/bigNum.h
@@ -0,0 +1,47 @@
+#ifndef LAB3C_BIGNUM_H
+#define LAB3C_BIGNUM_H
+
+#include <cmath>
+#include <sstream>
+#include <iostream>
+#include <limits>
+
+namespace Prog3b {
+
+    class bigNum{
+
+    private:
+        static const int SZ = 47;
+        char *Num;
+        int n; //кол-во разрядов
+
+    public:
+        bigNum();
+        bigNum(long int x);
+        bigNum(const char* str);
+        bigNum& Set(const char* str);
+        bigNum(bigNum&& t)  noexcept :n(t.n), Num(t.Num) { t.Num = nullptr; }
+        [[noreturn]] ~bigNum() { delete[]Num; }
+        bigNum(const bigNum&);
+        friend bigNum operator +(const bigNum& first, const bigNum& second);
+        friend bigNum operator -(const bigNum& first, const bigNum& second) { return first + (-second); };
+        bigNum operator -() const;
+        bigNum operator~() const;
+        bigNum& operator =(const bigNum&);
+        bigNum& operator =(bigNum&&) noexcept ;
+        bigNum& operator <<=(int);
+        bigNum& operator >>=(int);
+        friend bool operator >(const bigNum&, const bigNum&); //перегрузка друзьями
+        friend bool operator <(const bigNum&, const bigNum&);
+        friend bool operator ==(const bigNum&, const bigNum&);
+        friend std::ostream& operator <<(std::ostream&, const bigNum&);
+        friend std::istream& operator >>(std::istream&, bigNum&);
+        bool CompareAbs(const bigNum&) const;
+        operator int() const;
+        bigNum& resize(int, bool);
+    };
+    int digit_plus(const char *); //реализовать как внешнюю функции
+}
+
+
+#endif //LAB3C_BIGNUM_H
